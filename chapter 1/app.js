9941481128d4ff9f48b4d01fbdfe09ddf779886a
alishbarasheed254-lgtm <alishbarasheed254@gmail.com>
2026-07-23 03:19:35 +0500

    assignment done

---
+++ chapter 1/app.js
@@ -0,0 +1,17 @@
+    // question no 1
+// alert(" JS Alert")
+
+// question no 2
+    // alert("JavaScript Alert")
+
+
+// question no 3
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+// question no 4
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+
+// question no 5
+// alert("Hello... I can run JS through my web browser's console");
---
+++ chapter 2/app.js
@@ -0,0 +1,37 @@
+// question no 1
+// var username;
+
+// question no 2
+// var myName = "Alishba Rasheed";
+
+// question no 3
+// var message = "Hello World";
+// alert(message);
+
+// question no 4
+// var studentName = "Jhone Doe";
+// var age = "15 years old";
+// var course = "Certified Mobile Application Development";
+
+// alert(studentName);
+// alert(age);
+// alert(course);
+
+// question no 5
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(pizza);
+
+// question no 6
+// var email = "example@example.com";
+// alert("My email address is " + email);
+
+// question no 7
+//  var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
+
+// question no 8
+//  document.write("Yah! I can write HTML content through JavaScript");
+
+
+// question no 9
+//  alert ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
