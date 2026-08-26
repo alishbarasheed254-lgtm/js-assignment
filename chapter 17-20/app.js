16753d942d93264341f481b8667cec18483db0b6
alishbarasheed254-lgtm <alishbarasheed254@gmail.com>
2026-08-27 01:43:50 +0500

    done

---
+++ chapter 17-20/app.js
@@ -0,0 +1,109 @@
+// Q1
+
+// var arr = [
+//     [],
+//     [],
+//     []
+// ];
+
+// console.log(arr);
+
+
+// // Q3
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+
+// // Q4
+
+// var num = +prompt("Enter a number to show its multiplication table:");
+// var length = +prompt("Enter the length of table:");
+
+// for (var i = 1; i <= length; i++) {
+//     console.log(num + " x " + i + " = " + (num * i));
+// }
+
+
+// // Q5
+
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// for (var i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+// Q6
+
+// // a
+// for (var i = 1; i <= 15; i++) {
+//     console.log(i);
+// }
+
+// // b
+// for (var i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+
+// // c
+// for (var i = 0; i <= 20; i += 2) {
+//     console.log(i);
+// }
+
+// // d
+// for (var i = 1; i <= 19; i += 2) {
+//     console.log(i);
+// }
+
+// // e
+// for (var i = 2; i <= 20; i += 2) {
+//     console.log(i + "k");
+// }
+
+
+// // Q7
+
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+// var search = prompt("Enter item to search:");
+
+// if (A.includes(search)) {
+//     alert("Item is found in the list");
+// } else {
+//     alert("Item is not found in the list");
+// }
+
+
+// // Q8
+// let A = [24, 53, 78, 91, 12];
+// let largest = A[0];
+
+// for (let i = 1; i < A.length; i++) {
+//     if (A[i] > largest) {
+//         largest = A[i];
+//     }
+// }
+
+// console.log("Array items:", A);
+// console.log("The largest number is", largest);
+
+
+// // Q9
+// let B = [24, 53, 78, 91, 12];
+// let smallest = B[0];
+
+// for (let i = 1; i < B.length; i++) {
+//     if (B[i] < smallest) {
+//         smallest = B[i];
+//     }
+// }
+
+// console.log("Array items:", B);
+// console.log("The smallest number is", smallest);
+
+
+//  Q10
+// for (let i = 5; i <= 100; i += 5) {
+//     console.log(i);
+// }
