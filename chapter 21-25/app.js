ac2476a882c9439941d060aeec8d8c5d9a49e60d
alishbarasheed254-lgtm <alishbarasheed254@gmail.com>
2026-09-01 01:51:07 +0500

    doneit

--- chapter 21-25/app.js
+++ chapter 21-25/app.js
@@ -2,3 +2,81 @@
 // var lastName = prompt ("Enter your last name")
 // var fullName = firstName + " " + lastName;
 // alert("Hello " + fullName + "!"); 
+
+// Q2
+// var mobile = prompt("Enter your favorite mobile phone model:");
+
+// document.write("My favorite phone is: " + mobile + "<br>");
+// document.write("Length of string: " + mobile.length);
+
+
+// Q3
+// var word = "Pakistani";
+// var index = word.indexOf("n");
+
+// document.write("<br><br>");
+// document.write("String: " + word + "<br>");
+// document.write("Index of 'n': " + index);
+
+
+// Q4
+// var word2 = "Hello World";
+// var lastIndex = word2.lastIndexOf("l");
+
+// document.write("<br><br>");
+// document.write("String: " + word2 + "<br>");
+// document.write("Last index of 'l': " + lastIndex);
+
+
+// Q5
+// var word3 = "Pakistani";
+// var character = word3.charAt(3);
+
+// document.write("<br><br>");
+// document.write("String: " + word3 + "<br>");
+// document.write("Character at index 3: " + character);
+
+// Q7
+// var city = "Hyderabad";
+// var newCity = city.replace("Hyder", "Islam");
+
+// document.write("City: " + city + "<br>");
+// document.write("After replacement: " + newCity);
+
+
+ // Q8
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var newMessage = message.replace(/and/g, "&");
+
+// document.write("<br><br>");
+// document.write("Original message: " + message + "<br>");
+// document.write("After replacement: " + newMessage);
+
+
+// Q9
+var value = "472";
+var number = Number(value);
+
+document.write("<br><br>");
+document.write("Value: " + value + "<br>");
+document.write("Type: " + typeof value + "<br>");
+document.write("Value after conversion: " + number + "<br>");
+document.write("Type after conversion: " + typeof number);
+
+
+// Q10
+// var input = prompt("Enter a word:");
+// var upperCase = input.toUpperCase();
+
+// document.write("<br><br>");
+// document.write("User input: " + input + "<br>");
+// document.write("Upper case: " + upperCase);
+
+
+// Q11
+// var input2 = prompt("Enter a word:");
+// var titleCase = input2.charAt(0).toUpperCase() + input2.slice(1).toLowerCase();
+
+// document.write("<br><br>");
+// document.write("User input: " + input2 + "<br>");
+// document.write("Title case: " + titleCase);
