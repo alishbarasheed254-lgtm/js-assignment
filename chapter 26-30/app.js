8297b4513dcc6d637d0a7898c85bd700c33d39fb
alishbarasheed254-lgtm <alishbarasheed254@gmail.com>
2026-09-07 23:25:06 +0500

    doneit

---
+++ chapter 26-30/app.js
@@ -0,0 +1,64 @@
+// / Q1
+// var num = parseFloat(prompt("Enter a positive number:"));
+
+// document.write("Number: " + num + "<br>");
+// document.write("Round off value: " + Math.round(num) + "<br>");
+// document.write("Floor value: " + Math.floor(num) + "<br>");
+// document.write("Ceil value: " + Math.ceil(num) + "<br><br>");
+
+
+// Q2
+// var num = parseFloat(prompt("Enter a negative floating point number:"));
+
+// document.write("Number: " + num + "<br>");
+// document.write("Round off value: " + Math.round(num) + "<br>");
+// document.write("Floor value: " + Math.floor(num) + "<br>");
+// document.write("Ceil value: " + Math.ceil(num) + "<br><br>");
+
+
+// Q3
+// var num = parseFloat(prompt("Enter a number:"));
+
+// document.write("The absolute value of " + num + " is " + Math.abs(num) + "<br><br>");
+
+
+// Q4
+// var dice = Math.floor(Math.random() * 6) + 1;
+
+// document.write("Random dice value: " + dice + "<br><br>");
+
+
+// Q5
+// var coin = Math.floor(Math.random() * 2);
+
+// if (coin === 0) {
+//     document.write("Random coin value: Heads<br><br>");
+// } else {
+//     document.write("Random coin value: Tails<br><br>");
+// }
+
+
+// Q6
+// var randomNumber = Math.floor(Math.random() * 100) + 1;
+
+// document.write("Random number between 1 and 100: " + randomNumber + "<br><br>");
+
+
+// Q7
+// var weight = prompt("Enter your weight in kilograms:");
+
+// var parsedWeight = parseFloat(weight);
+
+// document.write("The weight of user is " + parsedWeight + " kilograms<br><br>");
+
+
+// Q8
+// var secretNumber = Math.floor(Math.random() * 10) + 1;
+
+// var userNumber = parseInt(prompt("Enter a number between 1 and 10:"));
+
+// if (userNumber === secretNumber) {
+//     document.write("Congratulations! You guessed the secret number.");
+// } else {
+//     document.write("Try again! The secret number was " + secretNumber + ".");
+// }
